4a18872de477b87ed07d9eaaae8e2a2bd799feb9
Abdelhadi Sabani <asabani@student.1337.ma>
2023-01-20 08:23:14 +0100

    chore: add parse_option function
    This function it will parse the command line using getopt function

---
+++ src/core/sio_cmdline_opts.cpp
@@ -0,0 +1,90 @@
+#include "sio_cmdline_opts.hpp"
+
+static void _display_version(void) {
+	cerr << "servio version: " << (NAME "/" VERSION) << endl;
+}
+
+static void _display_help(void) {
+	cerr << NAME << " version: " << (NAME "/" VERSION) << endl;
+	cerr << "Usage: " NAME " [-hvtT] [-c filename]" << endl;
+}
+
+static bool _test_configuration(const Config &config) {
+	bool success = config.syntaxOnly();
+	cerr << NAME ": configuration file " << config.getPath() << " test is " << (success ? "successful" : "failed") << endl;
+	return success;
+}
+
+static void _dump_configuration(const Config &config) {
+	if (_test_configuration(config)) {
+		config.displayContent();
+	}
+}
+
+static void _load_configuration(Config &config, const string &path) {
+	struct stat buf;
+
+	bzero(&buf, sizeof buf);
+	int ret = stat(path.c_str(), &buf);
+	if (S_ISDIR(buf.st_mode) || ret != 0) {
+		if (S_ISDIR(buf.st_mode))
+			errno = EISDIR;
+		cerr << NAME ": \"" << path << "\" failed : " << strerror(errno) << endl;
+	}
+	config.setPath(path);
+}
+
+bool parse_options(const int &ac, char *const *av, Config &config) {
+	int    opt, flags = 0;
+	string opt_name, path;
+
+	opterr = 0;  // suppress getopt error messages
+	while ((opt = getopt(ac, av, "hvtTc:")) != -1) {
+		switch (opt) {
+		case 'h':
+			flags |= HELP_OPT;
+			break;
+		case 'v':
+			flags |= VERSION_OPT;
+			break;
+		case 't':
+			flags |= CONF_TEST_OPT;
+			break;
+		case 'T':
+			flags |= CONF_DUMP_OPT;
+			break;
+		case 'c':
+			flags |= CONF_PATH_OPT;
+			path = optarg;
+			break;
+		case '?':
+			flags = UNKNOWN_OPT;
+			opt_name = string(1, optopt);
+			goto unknown;
+		}
+	}
+unknown:
+
+	if ((flags & UNKNOWN_OPT) || optind < ac) {
+		if (optind < ac && (flags & ~UNKNOWN_OPT))
+			opt_name = av[optind];
+		if (optopt == 'c')
+			cerr << NAME ": option \"-c\" requires file name" << endl;
+		else
+			cerr << NAME ": invalid option: \"" << opt_name << "\"\n";
+		return false;
+	} else if (flags & (VERSION_OPT | HELP_OPT | CONF_PATH_OPT | CONF_TEST_OPT | CONF_DUMP_OPT)) {
+		if (flags & VERSION_OPT)
+			_display_version();
+		if (flags & HELP_OPT)
+			_display_help();
+		if (flags & CONF_PATH_OPT)
+			_load_configuration(config, path);
+		if (flags & CONF_TEST_OPT)
+			_test_configuration(config);
+		if (flags & CONF_DUMP_OPT)
+			_dump_configuration(config);
+		return false;
+	}
+	return true;
+}
---
+++ src/core/sio_cmdline_opts.hpp
@@ -0,0 +1,22 @@
+#ifndef __CMDLINE_OPTIONS_H__
+#define __CMDLINE_OPTIONS_H__
+
+#include <libgen.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "servio.hpp"
+#include "sio_config.hpp"
+
+enum OptionTypes {
+	HELP_OPT = (1 << 1),
+	VERSION_OPT = (1 << 2),
+	CONF_TEST_OPT = (1 << 3),
+	CONF_DUMP_OPT = (1 << 4),
+	CONF_PATH_OPT = (1 << 5),
+	UNKNOWN_OPT = (1 << 6)
+};
+
+bool parse_options(const int &ac, char *const *av, Config &config);
+
+#endif

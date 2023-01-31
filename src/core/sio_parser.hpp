e5c0adc94b4c52087b02dc80781e6b009cda726c
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-01-31 19:22:48 +0100

    Add parser class

---
+++ src/core/sio_parser.hpp
@@ -0,0 +1,25 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include "sio_lexer.hpp"
+#include "sio_ast.hpp"
+
+class Parser {
+	string _serr;
+	Lexer _lex;
+
+   private:
+	
+   public:
+	Parser(ifstream &cfile) {
+		_lex.tokenizer(cfile);
+	};
+	MainContext *parse() {
+		return nullptr;
+	};
+	const string &err() const {
+		return _serr;
+	}
+};
+
+#endif

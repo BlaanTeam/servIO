8d8a7332d8459c76b5dcdf961604c2b68992a8f5
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-01-31 17:08:32 +0100

    Add syntax tree classes

---
+++ src/core/sio_ast.cpp
@@ -0,0 +1,60 @@
+#include "sio_ast.hpp"
+
+// MainContext functions
+
+MainContext::MainContext() {}
+
+MainContext::MainContext(const MainContext *copy) {
+	_directives = copy->_directives;
+}
+
+CtxType MainContext::type() {
+	return _type;
+}
+
+map<string, vector<string> > &MainContext::directives() {
+	return _directives;
+}
+
+vector<MainContext *> &MainContext::contexts() {
+	return _contexts;
+}
+
+void MainContext::addContext(MainContext *ctx) {
+	_contexts.push_back(ctx);
+}
+
+void MainContext::rmDirective(const string &dir) {
+	dirIter it = _directives.find(dir);
+	if (it != _directives.end()) _directives.erase(it);
+}
+
+vector<string> &MainContext::operator[](const string &dir) {
+	return _directives[dir];
+}
+
+MainContext::~MainContext() {
+	for (size_t idx = 0; idx < _contexts.size(); idx++)
+		delete _contexts[idx];
+};
+
+// HttpContext functions
+
+HttpContext::HttpContext() { _type = httpCtx; };
+
+HttpContext::HttpContext(const MainContext *copy)
+    : MainContext(copy) { _type = httpCtx; }
+
+// ServerContext functions
+
+ServerContext::ServerContext() { _type = serverCtx; };
+
+ServerContext::ServerContext(const MainContext *copy)
+    : MainContext(copy) { _type = serverCtx; }
+
+// LocationContext functions
+
+LocationContext::LocationContext() { _type = locationCtx; };
+
+LocationContext::LocationContext(const MainContext *copy)
+    : MainContext(copy) { _type = locationCtx; }
---
+++ src/core/sio_ast.hpp
@@ -0,0 +1,53 @@
+#ifndef __AST_H__
+#define __AST_H__
+
+#include <map>
+#include <vector>
+
+using namespace std;
+
+enum CtxType {
+	httpCtx,
+	serverCtx,
+	locationCtx,
+};
+
+class MainContext {
+   protected:
+	CtxType                                        _type;
+	vector<MainContext *>                          _contexts;
+	map<string, vector<string> >                   _directives;
+	typedef map<string, vector<string> >::iterator dirIter;
+
+   public:
+	MainContext();
+	MainContext(const MainContext *copy);
+
+	CtxType                       type();
+	map<string, vector<string> > &directives();
+	vector<MainContext *>        &contexts();
+
+	void            addContext(MainContext *ctx);
+	void            rmDirective(const string &dir);
+	vector<string> &operator[](const string &dir);
+
+	virtual ~MainContext();
+};
+
+class HttpContext : public MainContext {
+   public:
+	HttpContext();
+	HttpContext(const MainContext *copy);
+};
+
+class ServerContext : public MainContext {
+   public:
+	ServerContext();
+	ServerContext(const MainContext *copy);
+};
+
+class LocationContext : public MainContext {
+   public:
+	LocationContext();
+	LocationContext(const MainContext *copy);
+};

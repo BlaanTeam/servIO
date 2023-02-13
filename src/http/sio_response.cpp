699da070a9620cc26e4066f7bf66e72cda2129c7
Abdelhadi Sabani <asabani@student.1337.ma>
2023-02-13 09:08:55 +0100

    feat: add Response class

---
+++ src/http/sio_response.cpp
@@ -0,0 +1,70 @@
+#include "sio_response.hpp"
+
+Response::Response() {
+	init();
+	_keepAlive = true;
+}
+
+Response::Response(const short &statusCode, const string &contentType, bool keepAlive) {
+	init();
+	addHeader("Content-Type", contentType);
+	_statusCode = statusCode;
+	_keepAlive = keepAlive;
+}
+
+Response::Response(const Response &copy) {
+	(void)copy;
+}
+
+Response &Response::operator=(const Response &rhs) {
+	(void)rhs;
+	return *this;
+}
+
+void Response::init(void) {
+	// !INFO: for security reason we should hide this header in some cases !
+	addHeader("Server", NAME "/" VERSION);
+	addHeader("Date", getUTCDate());
+	addHeader("Connection", _keepAlive ? "keep-alive" : "close");
+	addHeader("Content-Type", DEFAULT_MIME_TYPE);
+}
+
+void Response::prepare(void) {
+	ss << HTTP_VERSION << " " << to_string(_statusCode) << " " << httpStatusCodes[_statusCode] << CRLF;
+
+	map<string, string>::iterator it = _headers.begin();
+
+	while (it != _headers.end()) {
+		ss << it->first << ": " << it->second << CRLF;
+		it++;
+	}
+}
+
+// Setters
+
+void Response::setStatusCode(const short &statusCode) {
+	_statusCode = statusCode;
+}
+
+void Response::addHeader(const string &name, const string &value) {
+	_headers[name] = value;
+}
+
+void Response::send(const sockfd &fd, iostream &stream, bool chunked) {
+	(void)chunked;  // TODO: add logic for chunked responses !
+
+	int seek = stream.tellg();
+	stream.seekg(0, stream.end);
+	addHeader("Content-Lenght", to_string(stream.tellg()));
+	stream.seekg(seek);
+
+	prepare();
+	::send(fd, ss.str().c_str(), ss.str().size(), 0);
+	::send(fd, CRLF, 2, 0);
+
+	while (stream) {
+		char buff[(1 << 10) + 1] = {0};
+		stream.read(buff, (1 << 10));
+		::send(fd, buff, stream.gcount(), 0);
+	}
+}
---
+++ src/http/sio_response.hpp
@@ -0,0 +1,49 @@
+#ifndef __RESPONSE_H__
+#define __RESPONSE_H__
+
+#define RES_INIT (1 << 0)
+#define RES_HEADER (1 << 1)
+#define RES_BODY (1 << 2)
+#define RES_DONE (1 << 3)
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+
+#include "./sio_http_codes.hpp"
+#include "./sio_mime_types.hpp"
+#include "utility/sio_socket.hpp"
+#include "utility/sio_utils.hpp"
+
+using namespace std;
+
+class Response {
+	stringstream ss;
+
+	short _statusCode;
+
+	map<string, string> _headers;
+	bool                _keepAlive;
+
+   public:
+	Response();
+	Response(const short &statusCode, const string &contentType = DEFAULT_MIME_TYPE, bool keepAlive = true);
+	Response(const Response &copy);
+	Response &operator=(const Response &rhs);
+
+	void init(void);
+	void prepare(void);
+
+	// Setters
+
+	void setStatusCode(const short &statusCode);
+
+	void addHeader(const string &name, const string &value);
+
+	void setConnectionStatus(bool keepAlive = true);
+
+	void send(const sockfd &fd, iostream &stream, bool chunked = false);
+};
+
+#endif

78f8f319ca172395346df08b954669d8a2f2160a
aklaikel <aklaikel@student.1337.ma>
2023-03-11 23:18:46 +0100

    add boundry file

---
+++ src/http/sio_boundary.hpp
@@ -0,0 +1,16 @@
+#ifndef __BOUNDRARY_H__
+#define __BOUNDRARY_H__
+
+#include <iostream>
+using namespace std;
+
+class Boundary {
+    
+
+   public:
+    Boundary();
+    ~Boundary();
+};
+
+
+#endif
